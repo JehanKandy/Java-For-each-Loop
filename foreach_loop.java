a48e352754a1d33d21c2ac3a1a74edc405f85b0b
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-05-02 21:24:32 +0530

    Add files via upload

---
+++ foreach_loop.java
@@ -0,0 +1,10 @@
+
+public class foreach_loop {
+    public static void main(String[] args) {
+        String [] foods = {"Egg","Bun","Rice","Water"};
+        for (String i : foods) {
+            System.out.println(i);
+        }
+    }
+    
+}
